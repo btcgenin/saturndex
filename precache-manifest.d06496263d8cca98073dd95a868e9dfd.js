26c7c149fd1cd532de79886705df52b620b8363b
btcgenin <68702427+btcgenin@users.noreply.github.com>
2020-07-23 21:53:52 +0530

    Add files via upload

---
+++ precache-manifest.d06496263d8cca98073dd95a868e9dfd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3be0bf0b57c00963df0f8c8247f60d47",
+    "url": "./index.html"
+  },
+  {
+    "revision": "525e279fee856f2ac8e9",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2335d3512d2dc8f3674c",
+    "url": "./static/css/main.68c2ced0.chunk.css"
+  },
+  {
+    "revision": "525e279fee856f2ac8e9",
+    "url": "./static/js/2.bc22c9d7.chunk.js"
+  },
+  {
+    "revision": "8f85096b1f7efb5c4f364c96955ed4b7",
+    "url": "./static/js/2.bc22c9d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2335d3512d2dc8f3674c",
+    "url": "./static/js/main.167c33db.chunk.js"
+  },
+  {
+    "revision": "63c9dbfb74b7006825a3",
+    "url": "./static/js/runtime-main.98c3ba2d.js"
+  }
+]);
